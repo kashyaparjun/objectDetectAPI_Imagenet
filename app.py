9b5b7b906dbe9c5bb57cbd785ac74e837096c6ce
kashyaparjun <arjuns062009@gmail.com>
2018-07-22 13:10:59 +0530

    hi

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template, request
 from werkzeug import secure_filename
 app = Flask(__name__)
 
+@app.route('/')
+def hi():
+    return "HI!"
+
 @app.route('/upload')
 def upload_file():
     return render_template('upload.html')
